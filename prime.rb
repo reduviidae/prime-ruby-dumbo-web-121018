faf731f8760bd078c9f38046b5cf66eeef1a3d14
Wil Cook <wil.cook.is@gmail.com>
2018-11-21 20:02:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,12 @@
 # Add  code here!
 def prime?(integer)
   x = 2 
+  isprime = true
   while (x <= integer / 2)
-    if integer % 2 == 0 
-      false 
+    if integer % x == 0 
+      isprme = false 
       break
-  
+      x += 1 
   end
   
 end
