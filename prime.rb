be32e9c4a798523c3bf96cb9f12d36c86fe2e100
Wil Cook <wil.cook.is@gmail.com>
2018-11-21 20:08:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+  x = 2 
+  isprime = true
+  while (x <= integer / 2)
+    if integer % x == 0 
+      isprime = false 
+      break
+      x += 1 
+  end
+end
+  
