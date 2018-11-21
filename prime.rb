80712b8cdf9aedc5b6dc1f79b7b64547149d556f
Wil Cook <wil.cook.is@gmail.com>
2018-11-21 19:57:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,6 @@
 # Add  code here!
 def prime?(integer)
-  if integer <=1 
-    return false 
-  elsif integer == 2 
-    return true  
-  elsif integer % 2 == 0 
-    return false
-  elsif integer == 3
-    return true 
-  elsif integer % 3 == 0 
-    return false 
-  elsif integer == 5
-    return true 
-  elsif integer % 5 == 0 
-    return false 
-  elsif integer % 9 == 0 
-    return false
-  else 
-    return true
-  end
+  x = 2 
+  while 
 end
   
